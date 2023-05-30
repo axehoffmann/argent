8a5dc3621845987f3b4f4f4b6e2f69fce34a578e
axelmhoffmann <79813818+axelmhoffmann@users.noreply.github.com>
2023-05-30 20:03:35 +0930

    Allowed input system to use enum as key

--- src/core/Input.h
+++ src/core/Input.h
@@ -6,6 +6,7 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+
 namespace ag
 {
 	enum class ActionType
@@ -16,34 +17,81 @@ namespace ag
 		Up /* True while button isn't held */
 	};
 
+	template <typename T>
 	class Input
 	{
 	public:
-		void RegisterAction(std::string& name, std::vector<int>& keys);
-		void RegisterAction(std::string& name, std::vector<int>& posKeys, std::vector<int>& negKeys);
+		void RegisterAction(T key, std::string& name, std::vector<int>& keys)
+		{
+			boolActions[key] = BoolAction(name, keys);
+		}
 
-		bool GetB(std::string& name, ActionType type);
-		float GetF(std::string& name);
+		void RegisterAction(T key, std::string& name, std::vector<int>& posKeys, std::vector<int>& negKeys)
+		{
+			axisActions[key] = AxisAction(name, posKeys, negKeys);
+		}
+
+		bool GetB(T key, ActionType type)
+		{
+			BoolAction* action = &boolActions[key];
+			for (size_t i = 0; i < action->keys.size(); i++)
+			{
+				if (PollFunc(action->keys[i], type))
+					return true;
+			}
+
+			return false;
+		}
+
+		float GetF(T key)
+		{
+			float negative = 0.0f;
+			float positive = 0.0f;
+
+			AxisAction* action = &axisActions[key];
+
+			for (size_t i = 0; i < action->positive.size(); i++)
+			{
+				if (PollFunc(action->positive[i], ActionType::Down))
+				{
+					positive = 1.0f;
+					break;
+				}
+			}
+
+			for (size_t i = 0; i < action->negative.size(); i++)
+			{
+				if (PollFunc(action->negative[i], ActionType::Down))
+				{
+					negative = 1.0f;
+					break;
+				}
+			}
+
+			return positive - negative;
+		}
 
 	private:
 		struct BoolAction
 		{
+			std::string name;
 			std::vector<int> keys;
 
-			BoolAction(std::vector<int>& k) { keys = k; }
+			BoolAction(std::string n, std::vector<int>& k) { name = n; keys = k; }
 		};
 		
 		struct AxisAction
 		{
+			std::string name;
 			std::vector<int> positive;
 			std::vector<int> negative;
 
-			AxisAction(std::vector<int>& p, std::vector<int>& n) { positive = p; negative = n; }
+			AxisAction(std::string n, std::vector<int>& p, std::vector<int>& ne) { name = n; positive = p; negative = ne; }
 		};
 
 		bool PollFunc(int key, ActionType type);
 
-		std::unordered_map<std::string, BoolAction> boolActions;
-		std::unordered_map<std::string, AxisAction> axisActions;
+		std::unordered_map<T, BoolAction> boolActions;
+		std::unordered_map<T, AxisAction> axisActions;
 	};
 }
