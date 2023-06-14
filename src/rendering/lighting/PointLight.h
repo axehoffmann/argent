1355904af2274efd89848f4a12d115df2deba3b7
axelmhoffmann <79813818+axelmhoffmann@users.noreply.github.com>
2023-06-14 21:11:48 +0930

    PointLight structure

---
+++ src/rendering/lighting/PointLight.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+
+namespace ag
+{
+	struct PointLight
+	{
+	public:
+		PointLight(glm::vec3 pos, glm::vec3 col, float intense) : position(pos), colour(col), intensity(intense) { }
+		glm::vec3 position;
+		glm::vec3 colour;
+		float intensity;
+	};
+}
