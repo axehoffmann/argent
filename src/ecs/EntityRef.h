f94cfc91fd74fc6a5621782e530dd4732000c762
axelmhoffmann <79813818+axelmhoffmann@users.noreply.github.com>
2023-07-26 21:35:30 +0930

    Persistent EntityRef implementation

---
+++ src/ecs/EntityRef.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "ECS.h"
+#include "ArchetypeCollection.h"
+
+namespace ag
+{
+	/// TODO: Do I want to template this on expected component types? Compile time cost = icky, but much safer code
+	class EntityRef
+	{
+	public:
+		EntityRef(EntityID entityID) : 
+			id(entityID), 
+			archetype(ag::ArchetypeCollection::GetArchetypeFromEntityID(entityID)),
+			index(-1) {}
+
+		bool Refresh()
+		{
+			index = archetype->GetIndexByID(id);
+			return index >= 0;
+		}
+
+        /**
+        * Get a reference to a component from this entity.
+        * @tparam The type of component to get.
+        * @return A reference to the selected component
+        */
+        template <typename C>
+        C& Get()
+        {
+            return archetype->GetComponent<C>(index);
+        }
+
+        /**
+        * Get a copy of the info struct of the entity.
+        */
+        EntityInfo Info() const
+        {
+            return archetype->GetEntityInfo(index);
+        }
+
+        /**
+        * Mark the entity to be destroyed next update.
+        */
+        void Destroy()
+        {
+            archetype->DestroyEntity(index);
+        }
+
+	private:
+		EntityID id;
+		std::shared_ptr<ag::ArchetypeCollection> archetype;
+		size_t index;
+	};
+}
