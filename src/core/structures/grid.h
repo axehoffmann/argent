febc27ccbcca9815b07435597856611d8dfa3a36
axehoffmann <79813818+axelmhoffmann@users.noreply.github.com>
2023-08-30 11:18:43 +0930

    trying to untangle templated lamdas

--- src/core/structures/grid.h
+++ src/core/structures/grid.h
@@ -13,6 +13,8 @@ namespace agt
 	template <typename value>
 	struct grid
 	{
+		using predicate_ptr = bool(*)(value, glm::vec2);
+
 		grid(int x, int y, float cell_size) : width(x), height(y), cellsize(cell_size)
 		{
 			data = new std::vector<node>*[x];
@@ -31,38 +33,43 @@ namespace agt
 			delete[] data;
 		}
 
-		std::vector<value> query_box(glm::vec2 p1, glm::vec2 p2, std::function<bool(grid<value>::node)> predicate = [](node x) -> bool { return true; })
+		std::vector<value> query_box(glm::vec2 p1, glm::vec2 p2, predicate_ptr predicate = static_cast<predicate_ptr>(default_predicate))
 		{
 			glm::ivec2 p1_grid = to_grid_coords(p1);
 			glm::ivec2 p2_grid = to_grid_coords(p2);
 			if (p1_grid.x > p2_grid.x) std::swap(p1_grid.x, p2_grid.x);
 			if (p1_grid.y > p2_grid.y) std::swap(p1_grid.y, p2_grid.y);
 
-			return query_box_filtered(p1_grid, p2_grid, predicate)
+			return query_box_filtered(p1_grid, p2_grid, predicate);
 		}
 
-		std::vector<value> query_circle(glm::vec2 p, float r, std::function<bool(grid<value>::node)> predicate = [](node x) { return true; })
+		std::vector<value> query_circle(glm::vec2 p, float r, predicate_ptr predicate = static_cast<predicate_ptr>(default_predicate))
 		{
 			glm::ivec2 p1_grid = to_grid_coords({ p.x - r, p.y - r });
 			glm::ivec2 p2_grid = to_grid_coords({ p.x + r, p.y + r });
 
-			return query_box_filtered(p1_grid, p2_grid, [=](node x) { return (glm::length(x.pos - p) <= r) && predicate(x) })
+			return query_box_filtered(p1_grid, p2_grid, [=](value v, glm::vec2 pos) { return (glm::length(pos - p) <= r) && predicate(v, pos); });
 		}
 
 		void insert(value val, glm::vec2 pos)
 		{
 			glm::ivec2 pos = to_grid_coords(pos);
-			data[pos.x][pos.y].push_back(node{ pos, val });
+			data[pos.x][pos.y].push_back(node{ pos, val }); 
 		}
 
 	private:
+		bool default_predicate(value val, glm::vec2 pos)
+		{
+			return true;
+		}
+
 		struct node
 		{
 			glm::vec2 pos;
 			value val;
 		};
 
-		std::vector<value> query_box_filtered(glm::ivec2 p1_grid, glm::ivec2 p2_grid, auto predicate)
+		std::vector<value> query_box_filtered(glm::ivec2 p1_grid, glm::ivec2 p2_grid, std::function<bool(value, glm::vec2)> predicate)
 		{
 			std::vector<value> out;
 			for (size_t x = p1_grid.x; x < p2_grid.x; x++)
@@ -88,7 +95,7 @@ namespace agt
 		bool in_bounds(glm::vec2 pos)
 		{
 			return (pos.x < 0 || pos.x > width ||
-				pos.y < 0 || pos.y > height)
+				pos.y < 0 || pos.y > height);
 		}
 
 		glm::ivec2 to_grid_coords(glm::vec2 pos)
