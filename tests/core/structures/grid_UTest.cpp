157ff151b2306199da2ac4b7219f94539aafb52d
axehoffmann <79813818+axelmhoffmann@users.noreply.github.com>
2023-08-31 12:17:14 +0930

    Circle query testing

--- tests/core/structures/grid_UTest.cpp
+++ tests/core/structures/grid_UTest.cpp
@@ -10,27 +10,27 @@ namespace grid_test
 		agt::grid<int> gr(5, 5, 1);
 
 		size_t count;
-		count = gr.query_box({ 0.0f, 0.0f }, { 2.0f, 2.0f }).size();
+		count = gr.query_box(glm::vec2{ 0.0f, 0.0f }, glm::vec2{ 2.0f, 2.0f }).size();
 		ag_expect(count == 0, "Expected 0 results when querying an empty grid, found {}", count);
 
 		gr.insert(1, { 1.0f, 1.0f });
-		count = gr.query_box({ 0.0f, 0.0f }, { 2.0f, 2.0f }).size();
+		count = gr.query_box(glm::vec2{ 0.0f, 0.0f }, glm::vec2{ 2.0f, 2.0f }).size();
 		ag_expect(count == 1, "Expected 1 result when querying an occupied grid, found {}", count);
 		// Outside our query zone
 		gr.insert(1, { 1.6f, 3.5f });
-		count = gr.query_box({ 0.0f, 0.0f }, { 2.0f, 2.0f }).size();
+		count = gr.query_box(glm::vec2{ 0.0f, 0.0f }, glm::vec2{ 2.0f, 2.0f }).size();
 		ag_expect(count == 1, "Expected 1 result when querying an occupied grid, found {}", count);
 
 		gr.insert(1, { 0.5f, 0.5f });
 		gr.insert(1, { 1.5f, 1.5f });
-		count = gr.query_box({ 0.0f, 0.0f }, { 2.0f, 2.0f }).size();
+		count = gr.query_box(glm::vec2{ 0.0f, 0.0f }, glm::vec2{ 2.0f, 2.0f }).size();
 		ag_expect(count == 3, "Expected 3 results when querying an occupied grid, found {}", count);
 
 		// Move query over
-		count = gr.query_box({ 1.5f, 1.5f }, { 4.0f, 4.0f }).size();
+		count = gr.query_box(glm::vec2{ 1.5f, 1.5f }, glm::vec2{ 4.0f, 4.0f }).size();
 		ag_expect(count == 2, "Expected 2 results when querying an occupied grid, found {}", count);
 
-		count = gr.query_box({ 1.6f, 1.6f }, { 4.0f, 4.0f }).size();
+		count = gr.query_box(glm::vec2{ 1.6f, 1.6f }, glm::vec2{ 4.0f, 4.0f }).size();
 		ag_expect(count == 1, "Expected 1 result when querying an occupied grid, found {}", count);
 	}
 
@@ -46,9 +46,31 @@ namespace grid_test
 		gr.insert(2, { 1.2f, 1.0f });
 		gr.insert(8, { 1.3f, 1.1f });
 		// Check with predicate for even numbers
-		count = gr.query_box({ 0.0f, 0.0f }, { 2.0f, 2.0f }, 
+		count = gr.query_box(glm::vec2{ 0.0f, 0.0f }, glm::vec2{ 2.0f, 2.0f },
 			[](auto x) { return x.val % 2 == 0; })
 			.size();
 		ag_expect(count == 2, "Expected 2 results when querying with predicate, found {}", count);
+	}
+
+	$Case(circle_query, grid)
+	{
+		agt::grid<int> gr(5, 5, 1);
+
+		size_t count;
+
+		// Empty
+		count = gr.query_circle(glm::vec2{ 1.0f, 2.0f }, 1).size();
+		ag_expect(count == 0, "Expected 0 results querying an empty grid, found {}", count);
+		// Miss
+		gr.insert(1, { 1.8f, 1.2f });
+		count = gr.query_circle(glm::vec2{ 1.0f, 2.0f }, 1).size();
+		ag_expect(count == 0, "Expected 0 results querying a miss on occupied grid, found {}", count);
+		// 2 Hits
+		gr.insert(1, { 0.5f, 2.5f });
+		gr.insert(1, { 1.0f, 1.5f });
+		count = gr.query_circle(glm::vec2{ 1.0f, 2.0f }, 1).size();
+		ag_expect(count == 2, "Expected 2 results querying an occupied grid, found {}", count);
+
+		gr.insert(1, { 2.0f, 2.5f });
 	}
 }
