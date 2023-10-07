a3cdaffb37ba7fc1f1956fbb0b3daa7c27373b38
axehoffmann <79813818+axelmhoffmann@users.noreply.github.com>
2023-10-07 17:51:38 +1030

    More resources v2 impl

--- src/resources/v2/Resources.h
+++ src/resources/v2/Resources.h
@@ -2,23 +2,38 @@
 
 #include <string>
 #include <atomic>
+#include <unordered_map>
+#include <vector>
+#include <functional>
+
+#include "debug/log/Log.h"
 
 namespace ag
 {
+	struct load_policy {};
+	struct async : public load_policy {};
+
+	template <typename T>
+	concept load_policy_t = std::is_base_of_v<load_policy, T>;
+
+	using resrc_id_t = uint8_t;
+
 	class Resource
 	{
 
 	};
 
 	template <class R>
-	concept Resource_T = std::is_base_of_v<Resource, R>;
+	concept resource_t = std::is_base_of_v<Resource, R>;
 
 	/**
 	* Describes a resource's location
 	*/
-	template <Resource_T T>
+	template <resource_t T>
 	struct resrc_specifier
 	{
+		friend class resrc_manager;
+
 		resrc_specifier(std::string fpath)
 		{
 			path = fpath;
@@ -28,12 +43,12 @@ namespace ag
 		std::string path;
 	};
 
-	template <Resource_T T>
+	template <resource_t T>
 	struct resrc_handle
 	{
 		bool is_loaded() const
 		{
-			return (*status) > 0;
+			return !error && (*status) > 0;
 		}
 
 	private:
@@ -42,7 +57,54 @@ namespace ag
 		* > 0 otherwise
 		*/
 		std::atomic<uint8_t>* status;
-
+		bool error;
 		T* data;
+	};
+
+
+	class resrc_manager
+	{
+	public:
+		template <load_policy_t P, resource_t T>
+		resrc_handle<T> load(resrc_specifier<T> spec, std::function<void(const T&)> callback)
+		{
+			/// TODO: Policies, async queue, etc
+			
+			/// TODO: typeids
+			constexpr resrcTID = 0;
+
+			auto it = resources.find(spec.path);
+
+			if (it != resources.end())
+			{
+				if (resources.at(it).typeID != resrcTID)
+				{
+					Log::Error(sfmt("Loaded resource with incorrect type at {}", spec.path));
+					return resrc_handle<T>{.status = nullptr, .error = true, .data = nullptr};
+				}
+
+				// Resource present already.
+				entry& resrc = resources.at(it);
+				/// TODO: callback may get dropped in some cases?
+				if (*status > 0)
+				{
+					callback(*static_cast<T*>(resrc.data));
+					return resrc_handle<T>{.status = resrc.status, .error = false, .data = static_cast<T*>(resrc.data)};
+				}
+			}
+		}
+
+	private:
+		using resrc_callback = std::function<void(void*)>;
+
+		struct entry
+		{
+			std::atomic<uint8_t> status;
+			void* data;
+			resrc_id_t typeID;
+			std::vector<resrc_callback> waitingCallbacks;
+		};
+		/// TODO: map not synchronised
+		std::unordered_map<std::string, entry> resources;
 	};
 }
